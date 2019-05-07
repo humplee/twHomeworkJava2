4600bce656161d19aac920a9e0af99c3ab6a4437
humplee <littleted1221@163.com>
2019-05-07 16:26:18 -0700

    All functions implemented

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,8 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class MyMap {
 
@@ -17,22 +16,68 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        for(int i=0;i<array.size();i++){
+            array.set(i,array.get(i)*3);
+        }
+        return array;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        ArrayList<String> result=new ArrayList<>();
+
+        for(int i=0;i<array.size();i++){
+            result.add(String.valueOf((char)((array.get(i).toString().charAt(0))-'1'+'a')));
+        }
+        return result;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        ArrayList<String> result=new ArrayList<>();
+
+
+        for(int i=0;i<array.size();i++){
+            ArrayList<Character> buffer=new ArrayList<>();
+            int num=array.get(i);
+            num--;
+            buffer.add((char)(num%26+'a'));
+            num/=26;
+            while(num>0){
+                num--;
+                buffer.add((char)(num%26+'a'));
+                num/=26;
+            }
+
+            char[]a=new char[buffer.size()];
+            for(int j=0;j<a.length;j++){
+                a[j]=buffer.get(a.length-j-1);
+            }
+            StringBuilder sb=new StringBuilder();
+            sb.append(a);
+            result.add(sb.toString());
+        }
+        return result;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        Comparator com = new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2) {
+                return o2- o1;
+            }
+        };
+        Collections.sort(array,com);
+        return array;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        Comparator com = new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2) {
+                return o1- o2;
+            }
+        };
+        Collections.sort(array,com);
+        return array;
     }
+
 }
+
+
--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,6 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Reduce {
@@ -13,42 +15,99 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        Comparator com = new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2) {
+                return o1- o2;
+            }
+        };
+        Collections.sort(arrayList,com);
+        return arrayList.get(arrayList.size()-1);
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        Comparator com = new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2) {
+                return o1- o2;
+            }
+        };
+        Collections.sort(arrayList,com);
+        return arrayList.get(0);
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        int sum=0;
+        for(int i=0;i<arrayList.size();i++){
+            sum+=arrayList.get(i);
+        }
+        return (double)sum/arrayList.size();
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        Comparator com = new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2) {
+                return o1- o2;
+            }
+        };
+        Collections.sort(arrayList,com);
+        if(arrayList.size()==0)return 0;
+        if(arrayList.size()%2!=0)return (double)arrayList.get(arrayList.size()/2);
+        else return ((double)arrayList.get(arrayList.size()/2-1)+(double)arrayList.get(arrayList.size()/2))/2;
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+        for(int i=0;i<arrayList.size();i++){
+            if(arrayList.get(i)%2==0)return arrayList.get(i);
+        }
+        return 0;
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        for(int i=0;i<arrayList.size();i++){
+            if(arrayList.get(i)%2==0)return i;
+        }
+        return -1;
     }
 
     public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        Comparator com = new Comparator<Integer>(){
+            public int compare(Integer o1, Integer o2) {
+                return o1- o2;
+            }
+        };
+        Collections.sort(arrayList,com);
+        Collections.sort(this.arrayList,com);
+        if(arrayList.size()!=this.arrayList.size())return false;
+        for(int i=0;i<arrayList.size();i++){
+            if(this.arrayList.get(i)!=arrayList.get(i))return false;
+        }
+        return true;
     }
 
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+//        Comparator com = new Comparator<Integer>(){
+//            public int compare(Integer o1, Integer o2) {
+//                return o1- o2;
+//            }
+//        };
+//        Collections.sort(arrayList,com);
+        if(arrayList.size()==0)return 0.0;
+        if(arrayList.size()%2!=0)return (double)arrayList.get(arrayList.size()/2);
+        else return ((double)arrayList.get(arrayList.size()/2-1)+(double)arrayList.get(arrayList.size()/2))/2;
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        int theOdd=-1;
+        for(int i=0;i<arrayList.size();i++){
+            if(arrayList.get(i)%2!=0)theOdd=arrayList.get(i);
+        }
+        return theOdd;
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        int theOdd=-1;
+        for(int i=0;i<arrayList.size();i++){
+            if(arrayList.get(i)%2!=0)theOdd=i;
+        }
+        return theOdd;
     }
 }
