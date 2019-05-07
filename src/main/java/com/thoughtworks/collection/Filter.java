f4adafd878df0a588ac3fc5d0342d2c16aa592c2
humplee <littleted1221@163.com>
2019-05-06 22:13:19 -0700

    filter/flaten Operator functions implementation

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,6 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -14,18 +16,41 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        ArrayList<Integer> result=new ArrayList<Integer>();
+        for(int i=0;i<array.size();i++){
+            if(array.get(i)%2==0)result.add(array.get(i));
+        }
+        return result;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        ArrayList<Integer> result=new ArrayList<Integer>();
+        for(int i=0;i<array.size();i++){
+            if(array.get(i)%3==0)result.add(array.get(i));
+        }
+        return result;
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        ArrayList<Integer> listTemp = new ArrayList<>();
+        ArrayList<Integer> firstTemp = new ArrayList<>(firstList);
+        ArrayList<Integer> secondTemp = new ArrayList<>(secondList);
+
+        for(int i=0;i<firstTemp.size();i++){
+            if(secondTemp.contains(firstTemp.get(i))){
+                listTemp.add(firstTemp.get(i));
+            }
+        }
+        return listTemp;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List listTemp = new ArrayList();
+        for(int i=0;i<array.size();i++){
+            if(!listTemp.contains(array.get(i))){
+                listTemp.add(array.get(i));
+            }
+        }
+        return listTemp;
     }
 }
--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Flaten {
@@ -12,10 +13,23 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        ArrayList<Integer> result=new ArrayList<>();
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                result.add(array[i][j]);
+            }
+        }
+        return result;
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        ArrayList<Integer> result=new ArrayList<>();
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                result.add(array[i][j]);
+            }
+        }
+        Filter filter=new Filter(result);
+        return filter.getDifferentElements();
     }
 }
